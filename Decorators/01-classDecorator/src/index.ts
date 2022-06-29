384f13b2cb5f62cb4b93c224169a0677ebaf5c5c
Youzege <youzegehq@gmail.com>
2022-06-29 10:00:44 +0800

    feat: factory decorator

--- Decorators/01-classDecorator/src/index.ts
+++ Decorators/01-classDecorator/src/index.ts
@@ -1,32 +1,22 @@
-// 类的 装饰器
-// 装饰器: 本身是一个函数
-// 装饰器的参数是 构造函数
-// 装饰器 通过 @ 符号 进行使用
-// 执行时机: 类创建之后执行，执行一次
-// 多个装饰器执行顺序，从右到左，从下到上
-
-function useDecoratorA (flag: boolean) {
-  if (flag) {
-    return function (constructor: any) {
-      constructor.prototype.getName = () => {
-        console.log('youzege')
+function useDecoratorA () {
+  return function<T extends new (...args: any[]) => any> (constructor: any) {
+    return class extends constructor {
+      name = 'youzegehq'
+      getName () {
+        this.name
       }
     }
-  } else {
-    return function (constructor: any) {}
   }
 }
 
-function useDecoratorB (constructor: any) {
-  console.log('h B')
-}
-function useDecoratorC (constructor: any) {
-  console.log('h C')
-}
+const User = useDecoratorA()(
+  class {
+    name: string
+    constructor (name: string) {
+      this.name = name
+    }
+  }
+)
 
-@useDecoratorA(false)
-@useDecoratorB
-class User {}
-
-const userA = new User()
-;(userA as any).getName()
+const userA = new User('youzege')
+console.log(userA.getName())
---
+++ Decorators/01-classDecorator/src/simple_decorator.ts
@@ -0,0 +1,32 @@
+// 类的 装饰器
+// 装饰器: 本身是一个函数
+// 装饰器的参数是 构造函数
+// 装饰器 通过 @ 符号 进行使用
+// 执行时机: 类创建之后执行，执行一次
+// 多个装饰器执行顺序，从右到左，从下到上
+
+// function useDecoratorA (flag: boolean) {
+//   if (flag) {
+//     return function (constructor: any) {
+//       constructor.prototype.getName = () => {
+//         console.log('youzege')
+//       }
+//     }
+//   } else {
+//     return function (constructor: any) {}
+//   }
+// }
+
+// function useDecoratorB (constructor: any) {
+//   console.log('h B')
+// }
+// function useDecoratorC (constructor: any) {
+//   console.log('h C')
+// }
+
+// @useDecoratorA(false)
+// @useDecoratorB
+// class User {}
+
+// const userA = new User()
+// ;(userA as any).getName()
